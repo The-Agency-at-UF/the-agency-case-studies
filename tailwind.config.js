84317740217844d7496f1a95d06a8a399650df66
isavic01 <152566339+isavic01@users.noreply.github.com>
2025-03-27 03:31:26 -0400

    Seagram case study layout and most animations-missing scrolling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        seagramgradientStart: "rgb(200, 215, 160, 0.2)", // #C8D7A0 with 20% opacity
+        seagramgradientEnd: "#000000", // Black
+      },
+      fontFamily: {
+        monserrat: ["Montserrat", "sans-serif"], // Replace "Montserrat" with your desired font
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        "fade-in": "fadeIn 1s ease-out forwards",
+      },
+    
+    },
   },
   plugins: [],
 }
