8e0d219b97e1b4be329f5f05407c9090455777fe
angelicahanley <hanleyangelica@ufl.edu>
2025-04-16 23:17:13 -0400

    Added AmazonAlexa case study branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // This line connects this front-end code to the server
 
 import GatorsUnidos from '../src/case-studies/GatorsUnidos';
+import AmazonAlexa from '../src/case-studies/AmazonAlexa';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/gatorsunidos" element={<GatorsUnidos />} />
+        <Route path="/amazonalexa" element={<AmazonAlexa />} />
         {/* Add other routes as needed */}
       </Routes>
     </Router>
---
+++ src/case-studies/AmazonAlexa.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AmazonAlexa = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>AmazonAlexa!!!</h1>
+      <p>Need assets</p>
+    </div>
+  );
+};
+
+export default AmazonAlexa;
